ba0b447129e86588d3c42f67e6dfa29c25635ad1
Valerie Reiss <127006059+ValerieReiss@users.noreply.github.com>
2023-03-18 10:10:12 +0100

    Add files via upload

--- GameScene.swift
+++ GameScene.swift
@@ -15,7 +15,7 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        //CGPointMake(0.5, 0.5)
+        CGPointMake(0.5, 0.5)
         
         let backgroundImage = SKSpriteNode(imageNamed: "backgroundImage")
         backgroundImage.position = .zero
--- GameScene1.swift
+++ GameScene1.swift
@@ -9,15 +9,9 @@ import SpriteKit
 import GameplayKit
 
 class GameScene1: SKScene {
-    
-    let swarms = Bundle.main.decode([Swarm].self, from: "swarms.json")
-    let fishTypes = Bundle.main.decode([FishType].self, from: "fish-types.json")
-    
-    var swarmNumber = 0
-    let positions = Array(stride(from: -320, through:320, by: 80))
-    
-    
+
     override func didMove(to view: SKView) {
+        CGPointMake(0.5, 0.5)
         physicsWorld.gravity = .zero
         
         let backgroundImage = SKSpriteNode(imageNamed: "backgroundAqua1")
@@ -68,104 +62,176 @@ class GameScene1: SKScene {
         
         
         
-        
-        
         //Bubbles
-        if let particles = SKEmitterNode(fileNamed: "Bubbles"){
-                    particles.position = CGPoint (x: 500, y: -50)
-                    particles.advanceSimulationTime(10)
-                    particles.zPosition = 5
-                    addChild(particles)
-                }
+        if let bubbles = SKEmitterNode(fileNamed: "Bubbles"){
+            bubbles.position = CGPoint (x: 500, y: -50)
+            bubbles.advanceSimulationTime(10)
+            bubbles.zPosition = 5
+            addChild(bubbles)
+            }
         
         //Medusa
-        if let particles = SKEmitterNode(fileNamed: "particleMedusa"){
-                    particles.position = CGPoint (x: 2000, y: -50)
-                    particles.advanceSimulationTime(10)
-                    particles.zPosition = 5
-                    addChild(particles)
-                }
-        //SwarmFishes
-        swarmFishes()
+        if let medusa = SKEmitterNode(fileNamed: "particleMedusa"){
+            medusa.position = CGPoint (x: 500, y: -50)
+            medusa.advanceSimulationTime(10)
+            medusa.zPosition = 5
+            addChild(medusa)
+        }
         
-        //SwarmMermaids
-        swarmMermaids()
+        //Seepferd
+        if let seepferd = SKEmitterNode(fileNamed: "particleSeepferd"){
+            seepferd.position = CGPoint (x: 2100, y: -50)
+            seepferd.advanceSimulationTime(10)
+            seepferd.zPosition = 5
+            addChild(seepferd)
+        }
         
-    }
-
-    func swarmFishes(){
+        
+        //Mermaid1
+        let mermaidArray = ["mermaid0", "mermaid1", "mermaid2"]
+        
+        if let mermaid0 = SKEmitterNode(fileNamed: "particleMermaid"){
+            //mermaid1.particleBirthRate = 0.07
+            mermaid0.particleTexture = SKTexture(imageNamed: mermaidArray[0])
+            mermaid0.position = CGPoint(x: 0, y: Int.random(in: 860..<960))
+            mermaid0.advanceSimulationTime(10)
+            mermaid0.zPosition = 5
+            addChild(mermaid0)
+        }
+        if let mermaid1 = SKEmitterNode(fileNamed: "particleMermaid"){
+            mermaid1.emissionAngle = 184
+            mermaid1.xAcceleration = -10
+            //mermaid1.particleBirthRate = 0.07
+            mermaid1.particleTexture = SKTexture(imageNamed: mermaidArray[1])
+            mermaid1.position = CGPoint(x: 2500, y: Int.random(in: 150..<250))
+            mermaid1.advanceSimulationTime(10)
+            mermaid1.zPosition = 5
+            addChild(mermaid1)
+        }
+        if let mermaid2 = SKEmitterNode(fileNamed: "particleMermaid"){
+            mermaid2.emissionAngle = 184
+            mermaid2.xAcceleration = -10
+            //mermaid1.particleBirthRate = 0.07
+            mermaid2.particleTexture = SKTexture(imageNamed: mermaidArray[2])
+            mermaid2.position = CGPoint(x: 2500, y: Int.random(in: 300..<500))
+            mermaid2.advanceSimulationTime(10)
+            mermaid2.zPosition = 5
+            addChild(mermaid2)
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        //FATFISH from left
+        
+        let fatFishArray = ["fish1", "fish2", "fish3", "fish4", "fish5", "fish6", "fish8"]
+        if let particles0 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles0.particleTexture = SKTexture(imageNamed: fatFishArray[0])
+                particles0.position = CGPoint (x: 0, y: Int.random(in: 760..<860))
+                particles0.advanceSimulationTime(10)
+                particles0.zPosition = 5
+                addChild(particles0)
+        }
+        if let particles1 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles1.particleBirthRate = 0.065
+                particles1.particleTexture = SKTexture(imageNamed: fatFishArray[1])
+                particles1.position = CGPoint (x: 0, y: Int.random(in: 900..<1100))
+                particles1.advanceSimulationTime(10)
+                particles1.zPosition = 5
+                addChild(particles1)
+        }
+        if let particles2 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles2.particleBirthRate = 0.01
+                particles2.particleTexture = SKTexture(imageNamed: fatFishArray[2])
+                particles2.position = CGPoint (x: 0, y: Int.random(in: 500..<600))
+                particles2.advanceSimulationTime(10)
+                particles2.zPosition = 5
+                addChild(particles2)
+        }
+        if let particles3 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles3.particleTexture = SKTexture(imageNamed: fatFishArray[3])
+                particles3.position = CGPoint (x: 0, y: Int.random(in: 300..<400))
+                particles3.advanceSimulationTime(10)
+                particles3.zPosition = 5
+                addChild(particles3)
+        }
+        if let particles4 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles4.particleTexture = SKTexture(imageNamed: fatFishArray[4])
+                particles4.position = CGPoint (x: 0, y: Int.random(in: 200..<300))
+                particles4.advanceSimulationTime(10)
+                particles4.zPosition = 5
+                addChild(particles4)
+        }
+        if let particles5 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles5.particleTexture = SKTexture(imageNamed: fatFishArray[5])
+                particles5.position = CGPoint (x: 0, y: Int.random(in: 400..<500))
+                particles5.advanceSimulationTime(10)
+                particles5.zPosition = 5
+                addChild(particles5)
+        }
+        if let particles6 = SKEmitterNode(fileNamed: "particleFatFish"){
+                particles6.particleBirthRate = 0.07
+                particles6.particleTexture = SKTexture(imageNamed: fatFishArray[6])
+                particles6.position = CGPoint (x: 0, y: Int.random(in: 150..<250))
+                particles6.advanceSimulationTime(10)
+                particles6.zPosition = 5
+                addChild(particles6)
+        }
+        
+        
+        //little swarm Fishes
         let swarmArray = ["particleSwarmfish", "particleSwarmfish1", "particleSwarmfish2", "particleSwarmfish3", "particleSwarmfish4"]
-
-        let timer = Timer.scheduledTimer(withTimeInterval: 40.0, repeats: true) { timer in
-                    let randomInt = Int.random(in: 0..<5)
-                    if let particles = SKEmitterNode(fileNamed: "particleSwarmfish"){
-                        particles.particleTexture = SKTexture(imageNamed: swarmArray[randomInt])
-                        particles.position = CGPoint (x: 0, y: Int.random(in: 100..<1000))
-                        particles.advanceSimulationTime(10)
-                        particles.zPosition = 10
-                        self.addChild(particles)
-                        print("geadded")
-                        let timer2 = Timer.scheduledTimer(withTimeInterval: 40.0, repeats: false) { timer2 in
-                            particles.removeFromParent()
-                            print("entfernt")
-                        }
-                    }
-            }
+        
+        if let swarm0 = SKEmitterNode(fileNamed: "particleSwarmfish"){
+            swarm0.particleBirthRate = 0.5
+            swarm0.particleTexture = SKTexture(imageNamed: swarmArray[2])
+            swarm0.position = CGPoint (x: 0, y: Int.random(in: 1000..<1200))
+            swarm0.advanceSimulationTime(10)
+            swarm0.zPosition = 3
+            addChild(swarm0)
+        }
+        if let swarm1 = SKEmitterNode(fileNamed: "particleSwarmfish"){
+            swarm1.particleTexture = SKTexture(imageNamed: swarmArray[1])
+            swarm1.position = CGPoint (x: 0, y: Int.random(in: 800..<900))
+            swarm1.advanceSimulationTime(10)
+            swarm1.zPosition = 3
+            addChild(swarm1)
+        }
+        if let swarm2 = SKEmitterNode(fileNamed: "particleSwarmfish"){
+            swarm2.particleTexture = SKTexture(imageNamed: swarmArray[3])
+            swarm2.position = CGPoint (x: 0, y: Int.random(in: 700..<800))
+            swarm2.advanceSimulationTime(10)
+            swarm2.zPosition = 3
+            addChild(swarm2)
+        }
+        if let swarm3 = SKEmitterNode(fileNamed: "particleSwarmfish"){
+            swarm3.particleTexture = SKTexture(imageNamed: swarmArray[4])
+            swarm3.position = CGPoint (x: 0, y: Int.random(in: 500..<600))
+            swarm3.advanceSimulationTime(10)
+            swarm3.zPosition = 3
+            addChild(swarm3)
+        }
+        if let swarm4 = SKEmitterNode(fileNamed: "particleSwarmfish"){
+            swarm4.particleBirthRate = 0.5
+            swarm4.particleTexture = SKTexture(imageNamed: swarmArray[0])
+            swarm4.position = CGPoint (x: 0, y: Int.random(in: 100..<200))
+            swarm4.advanceSimulationTime(10)
+            swarm4.zPosition = 3
+            addChild(swarm4)
+        }
+        
     }
-    
-    
-    
+
     override func update(_ currentTime: CFTimeInterval) {
-        for child in children {
-                    if child.frame.maxX < 0 {
-                        if !frame.intersects(child.frame){
-                            child.removeFromParent()
-                        }
-                    }
-                }
-        let activeFishes = children.compactMap { $0 as? FishNode}
-        if activeFishes.isEmpty {
-           // createSwarm()
-        }
-
     }
-    
-    func createSwarm() {
-            if swarmNumber == swarms.count {
-                //levelNumber += 1
-                swarmNumber = 0
-            }
-            
-            let currentSwarm = swarms[swarmNumber]
-            swarmNumber += 1
-            
-            //let maximumFishType = min(fishTypes.count, levelNumber + 1)
-            let fishType = Int.random(in:0...2)
-            
-            let fishOffsetX = CGFloat(Int.random(in:0...100))
-            let fishStartX = 2500
-            
-            if currentSwarm.fishes.isEmpty {
-                for(index, position) in positions.shuffled().enumerated(){
-                        let fish = FishNode(type: fishTypes[fishType], startPosition: CGPoint(x: fishStartX, y: position), xOffset: fishOffsetX * CGFloat(index * 2), moveStraight: true)
-                        addChild(fish)
-                }
-            } else {
-                for fish in currentSwarm.fishes{
-                    let node = FishNode(type: fishTypes[fishType], startPosition: CGPoint(x: fishStartX, y: positions[fish.position]), xOffset: fishOffsetX, moveStraight: fish.moveStraight)
-                    addChild(node)
-                }
-            }
-        }
-
-    
-    
-    
-    
+   
     
     func run(_ fileName: String){
                 run(SKAction.playSoundFileNamed(fileName, waitForCompletion: true))
     }
-    
     
 }
